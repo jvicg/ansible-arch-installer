49dc33067ee49d38c7d72753bf9802740cf5630c
nrk <jcorreag@pm.me>
2024-11-19 11:07:30 +0100

    add empty get_input.py for initial tests

---
+++ get_input.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# get_input.py
+# Script responsible of getting the user input and generate the variable files such as: ansible.cfg, inventory, etc.
+
+print("Generating user input")
